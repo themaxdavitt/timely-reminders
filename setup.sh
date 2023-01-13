a4eac4ad5e93943c3eb5ad05af1ebbfb562d8d16
Max Davitt <max@davitt.me>
2023-01-12 19:53:42 -0500

    Correct comment in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cat <<EOT >> /etc/crontabs/root
-# do daily human maintenance between (and including) 8am and 11pm :)
+# do daily human maintenance between 8am (inclusive) and 11pm (exclusive) :)
 */15    8-22    *       *       *       run-parts /srv/periodic-human/15min
 0       8-22    *       *       *       run-parts /srv/periodic-human/hourly
 
